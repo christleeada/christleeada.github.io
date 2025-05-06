84e03433f6d5108f9477ec6c61aa20b8c5c0a166
Christlee69 <89182731+Christlee69@users.noreply.github.com>
2025-05-06 13:23:55 +0800

    fix

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@ const toggle = document.querySelector('.dark-mode-toggle');
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
 });
+document.querySelector('.hamburger').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('active');
+    this.classList.toggle('active');
+});
